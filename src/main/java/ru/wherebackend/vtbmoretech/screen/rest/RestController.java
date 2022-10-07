0344e6b6dfe2d1418a48bebde44842133dd67923
alakey <prosoulk2017@gmail.com>
2022-10-08 02:02:11 +0800

    Adding rest controller

--- src/main/java/ru/wherebackend/vtbmoretech/screen/rest/RestController.java
+++ src/main/java/ru/wherebackend/vtbmoretech/screen/rest/RestController.java
@@ -1,15 +1,17 @@
 package ru.wherebackend.vtbmoretech.screen.rest;
 
 import io.jmix.core.DataManager;
+import io.jmix.core.FluentLoader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import ru.wherebackend.vtbmoretech.entity.Employee;
+import ru.wherebackend.vtbmoretech.entity.News;
 
 import java.util.UUID;
 
-@org.springframework.web.bind.annotation.RestController("vtbmt_RestController")
-@RequestMapping("/front/get")
+@org.springframework.web.bind.annotation.RestController
+@RequestMapping("/get")
 public class RestController {
 
     private DataManager dataManager;
@@ -17,7 +19,13 @@ public class RestController {
     @RequestMapping(value = "/getInfoEmployee",method = RequestMethod.GET)
     public Employee getEmployee(@RequestParam("userId") UUID userId) {
         return dataManager.load(Employee.class)
-                .id(userId)
+                .query("select e from vtbmt_Employee e where e.user.id = :userId")
+                .parameter("userId", userId)
                 .one();
+    }
+
+    @RequestMapping(value = "/getNews",method = RequestMethod.GET)
+    public FluentLoader.ByCondition<News> getNews() {
+        return dataManager.load(News.class).all();
     }
 }
