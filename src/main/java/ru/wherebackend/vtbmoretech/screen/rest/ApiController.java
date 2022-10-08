31ccbb6c2aebaf84f998897dace30908a73cb028
alakey <prosoulk2017@gmail.com>
2022-10-08 12:16:12 +0800

    adding history

--- src/main/java/ru/wherebackend/vtbmoretech/screen/rest/ApiController.java
+++ src/main/java/ru/wherebackend/vtbmoretech/screen/rest/ApiController.java
@@ -26,6 +26,7 @@ public class ApiController {
 
     String baseUrl = "https://hackathon.lsp.team/hk";
 
+    //Сохранение ключей
     private Employee getKeys() {
         UserDetails user = currentAuthentication.getUser();
         try {
@@ -52,7 +53,6 @@ public class ApiController {
                 employee.setPublicKey(publicKey);
                 employee.setPrivateKey(privateKey);
                 dataManager.save(employee);
-                return employee;
             } else {
                 System.out.println("Key already save");
             }
@@ -63,6 +63,7 @@ public class ApiController {
         return null;
     }
 
+    //Получение баланса
     @RequestMapping(value = "/getBalance",method = RequestMethod.GET)
     public String getBalance() {
         String publicKey = Objects.requireNonNull(getKeys()).getPublicKey();
@@ -72,12 +73,35 @@ public class ApiController {
             MediaType mediaType = MediaType.parse("text/plain");
             RequestBody body = RequestBody.create(mediaType, "");
             Request request = new Request.Builder()
-                    .url(baseUrl + "/v1/wallets/" + publicKey + "/balance")
+                    .url(baseUrl + "/v1/wallets/" + publicKey +"/balance")
                     .method("GET", body)
                     .build();
             Response response = client.newCall(request).execute();
             String responseData = response.body().string();
-            System.out.println(responseData);
+            response.close();
+            return responseData;
+        } catch (Exception e) {
+            e.getMessage();
+        }
+        return null;
+    }
+
+    //Получение истории
+    @RequestMapping(value = "/getHistory",method = RequestMethod.GET)
+    public String getHistory() {
+        String publicKey = Objects.requireNonNull(getKeys()).getPublicKey();
+        try {
+            OkHttpClient client = new OkHttpClient().newBuilder()
+                    .build();
+            MediaType mediaType = MediaType.parse("application/json");
+            RequestBody body = RequestBody.create(mediaType, "{\r\n  \"page\": 100,\r\n  \"offset\": 20,\r\n  \"sort\": \"asc\"\r\n}");
+            Request request = new Request.Builder()
+                    .url(baseUrl + "/v1/wallets/" + publicKey + "/history")
+                    .method("POST", body)
+                    .addHeader("Content-Type", "application/json")
+                    .build();
+            Response response = client.newCall(request).execute();
+            String responseData = response.body().string();
             response.close();
             return responseData;
         } catch (Exception e) {
